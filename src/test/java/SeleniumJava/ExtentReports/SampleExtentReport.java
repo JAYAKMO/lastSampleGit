78b6eb315e14cde00dd09dae2555b60e70ba2222
JAYAKMO <monisha.jai22@gmail.com>
2025-10-29 19:07:56 +0530

    last commit

---
+++ src/test/java/SeleniumJava/ExtentReports/SampleExtentReport.java
@@ -0,0 +1,39 @@
+package SeleniumJava.ExtentReports;
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class SampleExtentReport {
+	ExtentReports extent;
+	@BeforeTest
+	public ExtentReports getReportObject()
+	{
+		String path =System.getProperty("user.dir")+ "//reports//index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Results");
+		reporter.config().setDocumentTitle("Test Results");
+		
+		extent =new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Monisha");
+		return extent;
+			
+	}
+
+	@Test
+	public void Demo() {
+		extent.createTest("Demo");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in/");
+		driver.close();
+		extent.flush();
+	}
+}
